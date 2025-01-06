96a0953ac30c5e4e1ce76fbe71f8839a7dfac215
Bhupender Sharma <sharma05031989@gmail.com>
2025-01-07 00:04:40 +0530

    Added README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+Data for LLM : Convert Git repo to text
---
+++ git_ingest.py
@@ -0,0 +1,21 @@
+"""
+Data for LLM : Convert Git repo to text
+
+🚀 Exciting Tech Tool for all developers and AI enthusiast! 
+Git Ingest Codebase is here to revolutionize the way we analyze GitHub 
+repositories and prepare data for Language Models (LM). 🤖💻
+Git Ingest Codebase served as a powerful tool that converts 
+Git repositories into text that is easily digestible for LM. 
+By analyzing the file and directory structure, Git Ingest codebase
+ provides valuable statistical information such as file size and number of tokens. 📊
+What's even more impressive is that Git Ingest can be set up using 
+Docker or Python package manager, making it accessible to a wide range of users. 
+You're ready to start analyzing GitHub repositories. 🔄
+This tool is a game-changer for building code discovery tools, 
+chatbots, and various other applications. Not to mention, 
+it offers a detailed summary of the repository's structure and content, 
+which can be downloaded for further analysis. 📝
+
+
+
+"""
